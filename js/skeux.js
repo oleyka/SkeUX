8434b35dd9bfca297be72839783cd941439d0cd9
Olga Bobrova <oleyka@gmail.com>
2013-10-26 13:36:16 -0700

    Slider min/max/ticks

--- js/skeux.js
+++ js/skeux.js
@@ -1,3 +1,12 @@
+function skeux_max_width(el, subClass) {
+	var wSet = [];
+
+	$(el).find(subClass).each(function() {
+		wSet.push($(this).outerWidth());
+	});
+
+	return Math.max.apply(null, wSet);
+}
 
 function skeux_parse_nav_vertical() {
 	$('.skeux-tabs-nav-vertical').each(function() {
@@ -11,6 +20,56 @@ function skeux_parse_nav_vertical() {
 	});
 }
 
+function skeux_parse_buttonset_vertical() {
+	$('.skeux-buttonset-vertical').each(function() {
+		$(this).removeClass('ui-buttonset');
+		$(this).find('.ui-button').each(function() {
+			if ($(this).hasClass('ui-corner-left')) {
+				$(this).removeClass('ui-corner-left').addClass('ui-corner-top skeux-buttonset-vertical-button-first');
+			}
+			if ($(this).hasClass('ui-corner-right')) {
+				$(this).removeClass('ui-corner-right').addClass('ui-corner-bottom skeux-buttonset-vertical-button-last');
+			}
+			$(this).width(skeux_max_width(this, 'span'));
+		});
+		// $(this).width(skeux_max_width(this, '.ui-button span'));
+	});
+}
+
+function skeux_parse_slider_marks() {
+	$('.skeux-slider-marks').each(function() {
+		var min = $(this).slider('option', 'min');
+		var max = $(this).slider('option', 'max');
+		var step = $(this).slider('option', 'step');
+		if ($(this).slider('option', 'orientation') == 'horizontal') {
+			$(this).wrap('<div class="skeux-slider-horizontal-wrapper"></div>');
+		} else {
+			$(this).wrap('<div class="skeux-slider-vertical-wrapper"></div>');
+		}
+		$(this).after('<div class="skeux-slider-mark-min">' + min + '</div>');
+		$(this).after('<div class="skeux-slider-mark-max">' + max + '</div>');
+
+		var v = 1;
+		var c = parseInt((max-min)/(step*v));
+		var maxTicks = parseInt($(this).width() / 32);
+
+		while (c > maxTicks) {
+			c = parseInt((max-min)/(step*(++v)));
+		}
+
+		if (c > 0) {
+			var dTick = $(this).width()/c;
+			var xTick;		
+
+			while (c > 1) {
+				xTick = Math.round(dTick * (c-1));
+				$(this).after('<div class="skeux-slider-mark-tick" style="left: ' + (xTick-16) + 'px"></div>');
+			 	c--;
+			}
+		}
+	});
+}
+	
 function skeux_drop_corners() {
 	$('.skeux-no-corners').each(function() {
 		$(this).removeClass('ui-corner-all ui-corner-top ui-corner-bottom ui-corner-left ui-corner-right ui-corner-tl ui-corner-tr ui-corner-br ui-corner-bl');
@@ -46,6 +105,8 @@ function skeux_drop_corners() {
 
 function skeux_parse_all() {
 	skeux_parse_nav_vertical();
+	skeux_parse_buttonset_vertical();
+	skeux_parse_slider_marks();
 
 	skeux_drop_corners();
 }
